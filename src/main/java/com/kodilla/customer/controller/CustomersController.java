fb3853536575c51cc10665de86222a7e50782b63
Szymon Rosół <81100512+szymonprosol@users.noreply.github.com>
2022-02-15 20:32:17 +0100

    RefreshScope has been added

--- src/main/java/com/kodilla/customer/controller/CustomersController.java
+++ src/main/java/com/kodilla/customer/controller/CustomersController.java
@@ -5,10 +5,15 @@ import com.kodilla.customer.mapper.CustomerMapper;
 import com.kodilla.customer.service.DbService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
+@RefreshScope
 @RequestMapping("/v1/customers")
 @RequiredArgsConstructor
 public class CustomersController {
@@ -16,8 +21,15 @@ public class CustomersController {
     private final DbService service;
     private final CustomerMapper customerMapper;
 
+    @Value("${application.allow-get-customer}")
+    private boolean allowGetCustomers;
+
     @GetMapping(value = "/{customerId}")
     public CustomerDto getCustomerById(@PathVariable Long customerId) throws CustomerNotFoundException {
+        if(!allowGetCustomers) {
+            log.info("Getting customers is disabled");
+            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Getting customers is disabled");
+        }
         return customerMapper.mapToCustomerDto(
                 service.getCustomerById(customerId).orElseThrow(CustomerNotFoundException::new)
         );
